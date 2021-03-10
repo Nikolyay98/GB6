2dee147c3b27e5983c2c367ac2ba3f231aabe39d
Nikolay98 <nikolyay98@mail.ru>
2021-03-10 21:35:50 +0300

    dfgdg

--- src/Company/Animal.java
+++ src/Company/Animal.java
@@ -1,7 +1,6 @@
 package Company;
 public abstract class Animal
 {
-    // У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м.; прыжок: кот 2 м., собака 0.5 м.; плавание: кот не умеет плавать, собака 10 м.).
     protected float maxRunLength;
     protected float maxSwimLength;
     protected float maxJumpHeight;
